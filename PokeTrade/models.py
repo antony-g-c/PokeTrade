2e062d9dfc5eb071f120ee2d755fb84f84a718cd
Arthur Wang <awang728@gatech.edu>
2025-04-12 22:29:40 -0400

    basic Card model

---
+++ PokeTrade/models.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.db import models
+from django.contrib.auth.models import User
+
+from enum import Enum
+
+#Model for a Pokemon card
+class Card(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    pokemon = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    image = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.pokemon
---
+++ PokeTrade/views.py
@@ -0,0 +1,5 @@
+from .models import Card
+
+# home view
+def home(request):
+    
